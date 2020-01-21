802b851f1784887e4b3e9f837835b42d4c7ba3f4
madisonbrown <mbrown3391@gmail.com>
2020-01-21 14:05:35 -0500

    test

--- main.sh
+++ main.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 DIR=$(cd $(dirname $0) && pwd)
 NAME=$(basename $DIR)
-TARGET=$DIR/$NAME-$1
+TARGET=$DIR/deb/$NAME-$1
 #
-mkdir $TARGET & cp -a $DIR/src $TARGET
+[[ -d deb ]] || mkdir deb
+mkdir $TARGET && cp -a $DIR/src $TARGET
 cd $TARGET && dh_make --indep --createorig
-grep -v makefile debian/rules > debian/rules.new
-mv debian/rules.new debian/rules
-echo ./src/* ./ > debian/install
+echo './src/* ./' > debian/install
 rm debian/*.ex
-debuild
+#debuild
