e9fbf2f678d3a7c67461996c76d1b4ca41e04f1c
madisonbrown <mbrown3391@gmail.com>
2020-01-21 14:49:38 -0500

    test

--- main.sh
+++ main.sh
@@ -2,14 +2,16 @@
 DIR=$(cd $(dirname $0) && pwd)
 NAME=$(basename $DIR)
 #
-[[ -d deb ]] && rm -rf deb
-mkdir deb
+git merge working
 #
-TARGET=$DIR/deb/$NAME-$1 && mkdir $TARGET
+TARGET=$DIR/temp/$NAME-$1
+mkdir temp && mkdir $TARGET
 cd $TARGET && dh_make --indep --createorig
 echo './src/* ./' > debian/install
 echo '3.0 (native)' > debian/source/format
 rm debian/*.ex
-#
 cp -a $DIR/src $TARGET
 debuild
+#
+cd .. && mv *.deb /var/www/html
+cd .. && rm -rf temp
